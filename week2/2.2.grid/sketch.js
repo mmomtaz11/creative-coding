48937bb3a8044c3add6fbc866f3b2ec267cef9b6
mmomtaz11 <mmomtaz@mail.umw.edu>
2024-06-01 16:32:31 -0400

    Update sketch.js 2nd try

--- week2/2.2.grid/sketch.js
+++ week2/2.2.grid/sketch.js
@@ -1,7 +1,32 @@
 function setup() {
-  createCanvas(1200, 1200);
+  createCanvas(1400, 1400);
+  noLoop()
 }
 
 function draw() {
   background(220);
+
+  //push grid out of the corner
+  translate(180,100)
+  for (let x = 0; x < 8; x += 1){
+    for (let y = 0; y < 8; y += 1){
+      push();
+
+     //move the drawing plan to add a new shape
+      translate(x * 150, y * 150)
+
+      //line thickness and color
+      strokeWeight(10);
+      stroke(150, random(50,125),75);
+
+      //line color
+      fill(259,random(59,225), random(25,180));
+
+
+     //draw the shape
+      ellipse(0,0,random(42,150))
+
+      pop();
+    }
+  }
 }
