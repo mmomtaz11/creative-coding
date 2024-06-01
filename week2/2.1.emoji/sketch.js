1104713632407bb1cd08d239e663dab6d9db433b
mmomtaz11 <mmomtaz@mail.umw.edu>
2024-06-01 13:01:16 -0400

    Update sketch.js

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -4,4 +4,32 @@ function setup() {
 
 function draw() {
   background(220);
+
+//cheese wheel
+  fill(255,220,100); 
+  stroke("gold")
+  strokeWeight(10);
+  circle(200,200,250)
+
+//cheese hole 1
+  fill(255,300,100)
+  strokeWeight(7)
+  circle(200,150,50)
+
+//chesse hole 2
+  fill(255,300,100)
+  strokeWeight(7)
+  circle (120,160,40)
+
+//cheese hole 3
+  fill(255,300,100)
+  strokeWeight(10)
+  circle(150,250,110)
+
+//cheese hole 4
+  fill(255,300,100)
+  strokeWeight(7)
+  circle(275,200,75)
+
+
 }
