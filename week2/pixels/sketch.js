c9de4b519fc3afec57cd6b0f6bb3d7506b2d12f3
mmomtaz11 <mmomtaz@mail.umw.edu>
2024-06-06 15:54:06 -0400

    Update sketch.js

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,13 +1,33 @@
+
+let img;
+let sampleX = 25;
+let sampleY = 25;
+
 function preload(){
-	img = loadImage();
+	img = loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Anas_platyrhynchos_male_female_quadrat.jpg/800px-Anas_platyrhynchos_male_female_quadrat.jpg');
 }
 
 function setup() {
-  createCanvas(800, 800);
-  
+  createCanvas(900, 900);
+  img.loadPixels();
 }
 
 function draw() {
   background(220);
-image(img,10,10,100,100);
-}
+  noStroke()
+
+  translate(50,50);
+  //image(img,-2, -2);
+
+  for (let x = 0; x <= img.width; x += sampleX){
+    for (let y = 0; y <= img.height; y += sampleY){
+      var index = (x + y * img.width) * 4;
+      var r = img.pixels[index + 0];
+      var g = img.pixels[index + 1];
+      var b = img.pixels[index + 2];
+
+      fill(r,g,b);
+      ellipse(x,y,sampleX,sampleY);
+
+    }
+  }
